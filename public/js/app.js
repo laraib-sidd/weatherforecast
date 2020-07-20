4d118ea76f1e2bb988c5ec94a228d7eb120469b6
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-20 21:39:49 +0530

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,3 @@
-// let intt;
-// var inp = document.querySelector('input');
-// inp.addEventListener('input', (e) => {
-//     intt = e.target.value
-// })
-
-// var btn = document.querySelector('button');
-// btn.addEventListener('click', (e) => {
-// url = `http://localhost:3003/weather?address=d`
-// fetch(url)
-//     .then(res => res.json())
-//     .then((data) => {
-//         if (data.error) {
-//             console.log(data.error);
-//         } else {
-//             console.log(data);
-//         }
-//     })
-// })
 const para = document.querySelectorAll('p')
 
 var weatherForm = document.querySelector('form')
@@ -24,7 +5,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     para[1].innerText = "Loading...."
     const int = e.target[0].value
-    url = `http://localhost:3003/weather?address=${int}`
+    url = `/weather?address=${int}`
     fetch(url)
         .then(res => res.json())
         .then((data) => {
